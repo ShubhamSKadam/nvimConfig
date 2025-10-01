062d5582100a8b22f7a23431f1b8a44e387ad16f
ShubhamSKadam <shubhamskadam99@outlook.com>
2025-10-01 13:27:48 +0530

    Basic config and treeSitter config

---
+++ init.lua
@@ -0,0 +1,7 @@
+require("options")
+require("plugins")
+
+
+
+
+
---
+++ lua/options.lua
@@ -0,0 +1,23 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true 
+vim.opt.splitright = true 
+
+vim.opt.wrap = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Synchronizes the system clipboard 
+-- with Neovim's clipboard
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.virtualedit = "block"
+
+vim.opt.inccommand = "split"
+
+vim.opt.ignorecase = true
+
+vim.opt.termguicolors = true
