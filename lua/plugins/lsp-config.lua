d7699cedd10f7979ec9362277aa8188fedc12996
ShubhamSKadam <shubhamskadam99@outlook.com>
2025-10-05 01:34:30 +0530

    LSP config pesky bugs

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed ={ "lua_ls" }
+                ensure_installed ={ "lua_ls", "ts_ls"}
             })
         end
     },
@@ -18,6 +18,19 @@ return {
         config = function()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
+            lspconfig.ts_ls.setup({})
+            vim.keymap.set('n','K',vim.lsp.buf.hover,{})
+            vim.keymap.set('n','gd',vim.lsp.buf.definition,{})
+            vim.keymap.set({'n','v'},'<leader>ca', vim.lsp.buf.code_action,{})
+            vim.keymap.set('n', 'E', vim.diagnostic.open_float, {})
+
+            vim.diagnostic.config({
+                virtual_text = false, -- show error message on the line
+                signs = true,        -- show icon in the sign column
+                underline = true,    -- underline the error
+                update_in_insert = false,
+                severity_sort = true,
+    })
         end
     }
 }
