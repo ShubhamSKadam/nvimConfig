058e0fc849800a4597e3f654d2e584a1255e7ab0
ShubhamSKadam <shubhamskadam99@outlook.com>
2025-10-05 19:16:35 +0530

    Completions config added

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,35 +1,41 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim", config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed ={ "lua_ls", "ts_ls"}
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.ts_ls.setup({})
-            vim.keymap.set('n','K',vim.lsp.buf.hover,{})
-            vim.keymap.set('n','gd',vim.lsp.buf.definition,{})
-            vim.keymap.set({'n','v'},'<leader>ca', vim.lsp.buf.code_action,{})
-            vim.keymap.set('n', 'E', vim.diagnostic.open_float, {})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ts_ls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "E", vim.diagnostic.open_float, {})
 
-            vim.diagnostic.config({
-                virtual_text = false, -- show error message on the line
-                signs = true,        -- show icon in the sign column
-                underline = true,    -- underline the error
-                update_in_insert = false,
-                severity_sort = true,
-    })
-        end
-    }
+			vim.diagnostic.config({
+				virtual_text = false, -- show error message on the line
+				signs = true, -- show icon in the sign column
+				underline = true, -- underline the error
+				update_in_insert = false,
+				severity_sort = true,
+			})
+		end,
+	},
 }
