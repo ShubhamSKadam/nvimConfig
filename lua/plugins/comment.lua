ac6ae0b826f35fbd3ae71a44eb002f13b88de9f8
ShubhamSKadam <shubhamskadam99@outlook.com>
2025-10-22 18:05:20 +0530

    Comment plugin updated with the blend of treesitter logic

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,30 @@
 return {
+  {
     'numToStr/Comment.nvim',
-    opts = {},
+    opts = {
+      -- add any options here
+    },
     lazy = false,
+  },
+  {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    dependencies = {
+      'numToStr/Comment.nvim', -- Make sure Comment.nvim is loaded
+    },
+    config = function()
+      require('ts_context_commentstring').setup {
+        enable_autocmd = false, -- Important for perf
+      }
+
+      -- This is the critical part that connects the two plugins
+      local comment = require('Comment')
+      local ts_context = require('ts_context_commentstring.integrations.comment_nvim')
+
+      comment.setup {
+        -- This tells Comment.nvim to ask ts_context_commentstring
+        -- what comment string to use *before* it comments.
+        pre_hook = ts_context.create_pre_hook(),
+      }
+    end,
+  },
 }
